0a9a5bd40533401e9ab93b87acab2645d88db4a9
Atanas Chuchulev <atanas.v4@gmail.com>
2021-09-29 13:04:15 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "remote" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "atanas-test"
+  }
+}
